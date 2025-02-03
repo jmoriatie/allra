ad53dd7c628e7dde6993fa0db6048980733d199a
jeongmin.kim <jeongmin.kim@yanolja.com>
2025-02-04 06:15:49 +0900

    올라마켓 최초 커밋

---
+++ src/main/java/com/allar/market/MarketAllarApplication.java
@@ -0,0 +1,13 @@
+package com.allar.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketAllarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketAllarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/allar/market/MarketAllarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allar.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketAllarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
