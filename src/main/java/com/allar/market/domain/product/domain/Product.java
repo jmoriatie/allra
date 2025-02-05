9c4eb58476c6f266aae1793e820a8aece7a1cbaf
jeongmin.kim <jeongmin.kim@yanolja.com>
2025-02-06 07:24:55 +0900

    Product 도메인 엔티티 개발 ing & 공통 모듈 BaseEntity 작성

---
+++ src/main/java/com/allar/market/domain/product/domain/Product.java
@@ -0,0 +1,53 @@
+package com.allar.market.domain.product.domain;
+
+
+import com.allar.market.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+
+@Entity
+@Table(name = "product")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Product extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private int quantity;
+    private BigDecimal price;
+
+    @Builder
+    public Product(String name, int quantity, BigDecimal price) {
+        this.name = name;
+        this.quantity = quantity;
+        validatePrice(price);
+        this.price = price;
+    }
+
+    public void increaseQuantity(int quantity){
+        this.quantity = this.quantity + quantity;
+    }
+
+    public void decreaseQuantity(int quantity){
+        this.quantity = this.quantity + quantity;
+    }
+
+    public boolean hasEnoughQuantity(int quantity){
+        return this.quantity - quantity > 0;
+    }
+    private void validatePrice(BigDecimal price){
+        if(price == null)
+            throw new IllegalArgumentException("상품 금액을 입력해주세요.");
+
+        if(price.compareTo(BigDecimal.ZERO) < 0)
+            throw new IllegalArgumentException("상품 금액이 0보다 작을 수 없습니다.");
+    }
+}
---
+++ src/main/java/com/allar/market/domain/product/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.allar.market.domain.product.repository;
+
+import com.allar.market.domain.product.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
