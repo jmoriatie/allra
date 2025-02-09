0caec2c6b0974e3dce0ff17460e41c63834103d6
jeongmin.kim <jeongmin.kim@yanolja.com>
2025-02-09 16:33:31 +0900

    Customer 관련 필요없는 class 삭제 & Payment 디렉토리 변경

--- src/main/java/com/allar/market/application/payment/Payment.java
+++
@@ -1,8 +0,0 @@
-package com.allar.market.application.payment;
-
-public class Payment {
-
-    // TODO 필수! 결제완료? 장바구니 관련 로직 - 장바구니 어떻게?
-    //  ㄴ 결제시 Order를 통한 Product 감소, 장바구니 감소 또는 초기화
-    // TODO 대기중인 건들 find 로직, 변경 로직
-}
